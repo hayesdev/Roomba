22c999341ee04539b7d1f4a277f0ff14ebe81ee5
Greg Hayes <gthelight@gmail.com>
2020-12-14 14:32:02 -0600

    working on event handlers

--- roomba-project/src/components/Grid.js
+++ roomba-project/src/components/Grid.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 function renderGrid(num) {
+  function handleClick() {
+    const clickedSquare = document.getElementById("clicked");
+
+    clickedSquare.classList.toggle("clicked");
+  }
   const newGrid = [];
   for (let i = 1; i <= num; i++) {
     newGrid.push(
-      <div className="square" key={i}>
-        {i}
+      <div className="square" key={i} id="clicked" onClick={handleClick}>
+        {/* {i} */}
       </div>
     );
   }
